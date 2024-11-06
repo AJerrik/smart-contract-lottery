9c300d0c6ec59aad37cedf4fbe992c6bc7fa7802
AJerrik <ajerrik@gmail.com>
2024-11-06 18:19:00 +0000

    Added some tests

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -26,6 +26,7 @@ contract HelperConfig is CodeConstants, Script {
         uint32 callbackGasLimit;
         uint256 subscriptionId;
         address link;
+        address account;
     }
 
     NetworkConfig public localNetworkConfig;
@@ -60,7 +61,8 @@ contract HelperConfig is CodeConstants, Script {
                 gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
                 callbackGasLimit: 500000, // 500,000 gas
                 subscriptionId: 59285887267887144425932245852993527143240097417791541879307445156593607211950,
-                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+                account: 0x35a621562C80c82a8dC2042a047e6CAF0FA08556
             });
     }
 
@@ -88,7 +90,8 @@ contract HelperConfig is CodeConstants, Script {
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             callbackGasLimit: 500000, // 500,000 gas
             subscriptionId: 0, // might have to fix this
-            link: address(linkToken)
+            link: address(linkToken),
+            account: 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38
         });
         return localNetworkConfig;
     }
